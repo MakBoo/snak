e033c35d8eb569734da47f882b9920883f704718
MakBoo <m.s.terentjev@gmail.com>
2016-06-08 16:20:35 +0300

    lvl 1

---
+++ Snak/Snak/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hey Myster!");
+            Console.ReadLine();
+        }
+    }
+}
